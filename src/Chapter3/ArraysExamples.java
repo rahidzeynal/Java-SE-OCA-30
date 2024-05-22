d6608414e1d3ac9ab8a7b5be44cbce7775d3a599
Rahid Zeynalov <zeynalovrahid1@gmail.com>
2024-05-22 16:22:46 +0400

    Added new file related to arrays

--- src/Chapter3/ArraysExamples.java
+++ src/Chapter3/ArraysExamples.java
@@ -6,15 +6,24 @@ public class ArraysExamples {
     public static void main(String[] args) {
 
         // Sorting numeric array
+        System.out.print("Unsorted version of our array: ");
         int[] arrayInt = {10, 4, 6, 7, 3, 9, 8, 2, 1, 5};
         for (int i = 0; i < arrayInt.length; i++) {
             System.out.print(arrayInt[i] + " ");
         }
         System.out.println();
 
+        System.out.print("Sorted version of our array: ");
         Arrays.sort(arrayInt);
         for (int i = 0; i < arrayInt.length; i++) {
             System.out.print(arrayInt[i] + " ");
         }
+        System.out.println();
+        int sum = 0;
+        // Summing values of arrayInt
+        for (int i = 0; i < arrayInt.length; i++) {
+            sum += arrayInt[i];
+        }
+        System.out.println("Sum: " + sum);
     }
 }
