4d406acd6dfaad17750c7e888ac8a49bb022d40a
Rahid Zeynalov <zeynalovrahid1@gmail.com>
2024-05-27 22:49:45 +0400

    ----

--- src/Chapter3/ArrayDemo.java
+++ src/Chapter3/ArrayDemo.java
@@ -3,12 +3,15 @@ package Chapter3;
 public class ArrayDemo {
 
     public static void main(String[] args) {
+
+        //The most common way to create an array looks like this:
         int[] array = new int[3];
         array[0] = 1;
 //        array[1] = 2;
         array[2] = 3;
         System.out.println(array[1]); //It will print 0 as default value for int in arrays.
 
+        //Actually new int[] part is count as redundant by Java.
         int [] array2 = new int[] {1,2,3};
         System.out.println(array2[2]);
 
@@ -24,7 +27,6 @@ public class ArrayDemo {
 
 
         String [] bugs = {"cricket", "beetle", "ladybug"};
-
         String[] alias = bugs;
         System.out.println(bugs.equals(alias));
         System.out.println(bugs==alias);
@@ -41,5 +43,10 @@ public class ArrayDemo {
 
         System.out.println(array2.length);
         System.out.println(array2[1]);
+
+
+        String [] words = new String[] {"Some", "words."};
+        String [] words2 = new String[] {"Some", "words."};
+        System.out.println(words.equals(words2)); // Will return false because it looks for references are same or not
     }
 }
